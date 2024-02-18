2955cf99daf5f01b828de8cecdb4cb4021b1982b
picklerules <136508588+picklerules@users.noreply.github.com>
2024-02-17 19:41:29 -0500

    ajout message succes ajout panier

--- leonardo/resources/views/master.blade.php
+++ leonardo/resources/views/master.blade.php
@@ -54,7 +54,12 @@
                 </div>
             </div>
         </header>
-
+        @if(session('success'))
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            {{session('success')}}
+        </div>
+        @endif
         <!--Content-->
         @yield('content')
 
